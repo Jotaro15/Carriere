7da93b9166abe34f92cf674bcedeeb3b3482604a
Jotaro15 <sterenn.alana@gmail.com>
2023-10-30 10:50:51 +0100

    ajout de code back
    Dans Mine.php, base.blade.php et welcome.blade.php

--- app/Models/Mine.php
+++ app/Models/Mine.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Mine extends Model
 {
     use HasFactory;
+
+
+    public function concession(): BelongsTo
+    {
+        return $this->belongsTo{ related: Concession;;class};
+    }
+
 }
