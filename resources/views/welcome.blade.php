c93336515cb7524fad94ea437044581829ca753d
Jotaro15 <sterenn.alana@gmail.com>
2023-12-21 09:01:22 +0100

    update du welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <p>Au cœur de l'histoire minière de la région, le site minier de Pont-Péan est une vitrine du
         patrimoine industriel qui a façonné notre communauté depuis des décennies. Notre équipe
         d'administration est fière de jouer un rôle clé dans la préservation, la gestion et la mise 
-        en valeur de ce site historique.<br>
+        en valeur de ce site historique.<br> Ici on voit qu'il y a un problème. 
         Notre mission est de préserver l'authenticité du site minier, de garantir sa sécurité et de
         fournir des informations éducatives et culturelles aux visiteurs. À travers une gestion
         responsable, nous nous engageons à maintenir la mémoire de l'exploitation minière vivante et
